5c3d756f74be64dc54f5c14a84411495bef17315
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-06-09 12:38:01 -0700

    commit tree 746a7a38f5100f99ddd64d8f0a41d10c6e1efebf

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
 function distanceFromHqInBlocks(distance){
-    if (distance > 42){
-        let blocks = distance - 42;
-        return blocks;
-    } else {
-        let blocks = 42 - distance
-        return blocks;
+    if (distance < 42){
+        return 42 - distance
+    } else if (distance > 42){
+        return distance - 42
     }
-} 
+}
 
 function distanceFromHqInFeet(distance){
-    if (distance > 42){
-        let feet = ((distance - 42) * 264);
-        return feet;
-    } else {
-        let feet = ((42 - distance) * 264);
-        return feet;
+    if (distance < 42){
+        return (42 - distance) * 264
+    } else if(distance > 42){
+        return (distance - 42) * 264
     }
 }
 
 function distanceTravelledInFeet(start, destination){
     if (start < destination) {
-        return (destination - start) * 264;
+        return (destination - start) * 264
     } else {
-        return (start - destination) * 264;
-    }   
+        return (start - destination) * 264
+    }
 }
 
 function calculatesFarePrice(start, destination){
-    const distance = distanceTravelledInFeet(start, destination);
-        if (distance <= 400) {
-            return 0;
-        } else if (distance > 400 && distance <= 2000){
-            return (distance - 400) * .02
-        } else if (distance > 2000 && distance < 2500){
-            return 25 
-        } else if (distance > 2500){
-            return 'cannot travel that far'
-        }
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance < 400){
+        return 0
+    } else if (distance > 400 && distance < 2000){
+        return (distance - 400) * .02
+    } else if (distance > 2000 && distance < 2500){
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
     }
